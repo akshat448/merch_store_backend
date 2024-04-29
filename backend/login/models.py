c6f0c6de095237ac6655dafd10722295a53c491d
Vahgar8work <156514793+Vahgar8work@users.noreply.github.com>
2024-04-29 17:03:37 +0530

    jhjh

--- backend/login/models.py
+++ backend/login/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    Phone_Num = models.PositiveIntegerField(default=0 , unique=True)
+    First_Name = models.CharField(max_length=100)
+    Last_Name = models.CharField(max_length=100)
+    
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- backend/login/views.py
+++ backend/login/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+# from django.shortcuts import render, redirect
+# from django.contrib.auth.models import auth
+# from django.contrib import messages
 
-# Create your views here.
+# def login(request):
+#     '''login'''
+#     if request.method == 'POST':
+#         email = request.POST['email']
+#         pasword = request.POST['pasword']
+
+#         #user authentication using email
+#         user = auth.authenticate(email = email , password = pasword)
+#         if user!= None:
+#             auth.login(request, user)
+#             return redirect('/')            # redirect to home
+#         else:
+#             messages.warning(request , 'Invalid Credentials')
+#             return redirect('/login')       #refresh page and display message
+#     else:
+#         return render(request , 'login.html')  #first time login session
+    
+# def logout(request):
+#     '''logout'''
+#     auth.logout(request)
+#     return redirect('/')
+
+# def change_pass(request):
+#     '''change password'''
+#     if request.method == "POST":
+
+#         user = request.user
+#         old_password = request.old_pass
+#         new_password = request.new_pass
+#         confirm_password = request.confirm_pass
+
+#         if new_password != confirm_password:
+#             messages.warning(request , "Please match the passwords and try again")
+        
+        
+#     else:
+#         return render(request , 'change_password.html')
+
+# def register(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         email = request.POST['emailid']
+#         mobile_num = request.POST['mobile_num']
+#         password = request.POST['pasword']
+#         password2 = request.POST['pasword2']
+        
+#         if password != password2:
+#             messages.warning(request , "Please match the passwords and try again")
+#             return redirect('/register')
+#         elif User.objects.filter(email=email).exists():
+#             messages.info(request , "Email already exists")
+#             return redirect('/register')
+#         elif User.objects.filter(username=username).exists():
+#             messages.info(request , "This username already exists, please choose a different one")
+#             return redirect('/register')
+#         else:
+#             User.objects.create_user(username=username , password=password , email=email, mobile_num = mobile_num)
+#             return redirect('/login')
+#     else:
+#         return render(request, 'register.html')
